6ccc123d41809493066ff4f5ecfce4f73dde09bc
hamza mir <hamxa46.mir@gmail.com>
2017-08-25 11:19:23 +0500

    add tests for GET/todos

--- server/server.js
+++ server/server.js
@@ -1,41 +1,36 @@
-var express=require('express');
-var bodyParser=require('body-parser');
-var {mongoose}=require('./db/mongoose');
-var {Todo}=require('./models/todo');
-var {User}=require('./models/user');
-var app=express();
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
-var todo=new Todo({
-  text:req.body.text,
-  completed:req.body.completed
-});
-todo.save().then((doc)=>{
-  res.send(doc);
-},(e)=>{
-  console.log(e);
-})
-});
-
-app.get('/todos',(req,res)=>{
-  Todo.find().then((todos)=>{
-    res.send({todos});
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
   });
-},(e)=>{
-  res.staus(400).send(e);
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-
-
-
-app.listen(3000,(err)=>{
-  if(err){
-   console.log(err);
- }else{
-   console.log('listining on port 3000');
- }
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-module.exports={app};
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+module.exports = {app};
